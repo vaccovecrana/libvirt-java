6fc7a703b31fcc77a41c86e76e7f96e2478a51fe
Jesus Zazueta <jjzazuet@vacco.io>
2021-05-11 05:11:56 -0400

    - Library updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ plugins { id("io.vacco.oss.gitflow") version "0.9.7" }
 group = "io.vacco.libvirt"
 version = "0.5.2"
 
-configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
-  sharedLibrary(true, true)
+configure<io.vacco.oss.gitflow.GsPluginProfileExtension> { sharedLibrary(true, true) }
+
+val api by configurations
+
+dependencies {
+  api("net.java.dev.jna:jna:5.8.0")
 }
